03b56ee0c242c8ed0d5680f0278d9c821552cf4f
Raphael De Lio <raphadlts@gmail.com>
2023-06-23 09:42:47 +0200

    Fixing get top 10 flights HTTP method from POST to GET

--- TicketAnalyzer/src/main/kotlin/com/raphaeldelio/ticketanalyzer/analysis/BoardingPassAnalysisController.kt
+++ TicketAnalyzer/src/main/kotlin/com/raphaeldelio/ticketanalyzer/analysis/BoardingPassAnalysisController.kt
@@ -2,7 +2,7 @@ package com.raphaeldelio.ticketanalyzer.analysis
 
 import com.raphaeldelio.ticketanalyzer.analysis.flightnumber.FlightNumberAnalysis
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -10,7 +10,7 @@ class BoardingPassAnalysisController(
     private val boardingPassAnalysisService: BoardingPassAnalysisService
 ) {
 
-    @PostMapping("passengers/top-10")
+    @GetMapping("flights/top-10")
     fun getTop10Flights(): ResponseEntity<List<FlightNumberAnalysis?>> =
         ResponseEntity.ok(
             boardingPassAnalysisService.getTop10Flights()
